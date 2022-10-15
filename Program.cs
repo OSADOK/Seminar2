a353af74587df813ae260dffc7a4e038e4e7588c
EREMA <frank.eremin@yandex.com>
2022-10-15 15:15:57 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -16,25 +16,61 @@ Console.WriteLine($"{auf}");
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.Clear();
+// Console.WriteLine("Введите чило:");
+// int num = int.Parse(Console.ReadLine());
+// if (num < 100)
+// {
+// int n1 = (num % 100) / 10;
+// Console.WriteLine("Третьей цифры нет");
+// }
+// else if (num>=1000 && num<10000)
+// {
+// int a1 = num / 10;
+// int a2 = a1 % 10;    
+// Console.WriteLine($"{a2}");
+// }
+// else if (num>=10000)
+// {
+// int a1 = num / 100;
+// int a2 = a1 % 10;    
+// Console.WriteLine($"{a2}");
+// }
+// else
+// {
+// int a1 = num % 10;
+// int a2 = a1 % 10;    
+// Console.WriteLine($"{a2}");
+// }
+
+
+// Console.Clear();
+//     Console.WriteLine("Введите чило:");
+//     int num = int.Parse(Console.ReadLine());
+//     if (num < 100)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//     }
+//     else
+//     {
+//         Console.WriteLine(Convert.ToString(num)[2]);
+//     }
+
+// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
 Console.Clear();
-Console.WriteLine("Введите чило:");
-int num = int.Parse(Console.ReadLine());
-if (num < 100)
+Console.WriteLine("Введите чило от 1 до 7:");
+int a = int.Parse(Console.ReadLine());
+if (a == 7 || a == 6)
 {
-    int n1 = (num % 100) / 10;
-    Console.WriteLine("Третьей цифры нет");
+Console.Write($"Под цифрой{a}да, выходной");
 }
-else {
-    for(num>=1000);
-    int z = (num % 100) / 10;
-    Console.WriteLine($"{z}");
-    }
-    
-
-// }
-//         else {
-
-//         // int n3 = num%10;
-//         // Console.WriteLine($"{n3}");
-// }
-
+else
+{
+Console.Write($"Под цифрой {a}нет, не выходной");
+}
